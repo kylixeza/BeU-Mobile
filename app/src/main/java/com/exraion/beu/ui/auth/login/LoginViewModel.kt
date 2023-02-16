a5fa70faafe99d4e1b18167b6c8ff6572550d4bd
ACER <loilyxtra@gmail.com>
2023-02-16 23:26:18 +0700

    added auth screens

---
+++ app/src/main/java/com/exraion/beu/ui/auth/login/LoginViewModel.kt
@@ -0,0 +1,7 @@
+package com.exraion.beu.ui.auth.login
+
+import androidx.lifecycle.ViewModel
+
+class LoginViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/exraion/beu/ui/auth/register/RegisterViewModel.kt
@@ -0,0 +1,7 @@
+package com.exraion.beu.ui.auth.register
+
+import androidx.lifecycle.ViewModel
+
+class RegisterViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
