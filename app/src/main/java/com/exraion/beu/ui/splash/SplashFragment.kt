cffe819a3269c80902f3322cfa7dc81c808d7a44
ACER <loilyxtra@gmail.com>
2023-02-16 23:58:57 +0700

    designed splash screen

--- app/src/main/java/com/exraion/beu/ui/splash/SplashFragment.kt
+++ app/src/main/java/com/exraion/beu/ui/splash/SplashFragment.kt
@@ -2,6 +2,8 @@ package com.exraion.beu.ui.splash
 
 import android.view.ViewGroup
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.findNavController
+import com.exraion.beu.R
 import com.exraion.beu.base.BaseFragment
 import com.exraion.beu.databinding.FragmentSplashBinding
 import com.exraion.beu.util.ScreenOrientation
@@ -18,21 +20,22 @@ class SplashFragment : BaseFragment<FragmentSplashBinding>() {
     override fun FragmentSplashBinding.binder() {
         lifecycleScope.launchWhenStarted {
             viewModel.uiState.collect {
-                when (it) {
-                    SplashUIState.FirstRun -> {
-                        // Navigate to onboarding
-                    }
-                    SplashUIState.NotFirstRun -> {
-                        // Navigate to login
-                    }
-                    SplashUIState.LoggedIn -> {
-                        // Navigate to home
-                    }
-                    SplashUIState.NotLoggedIn -> {
-                        // Navigate to login
-                    }
-                    else -> {
-
+                view?.apply {
+                    when (it) {
+                        SplashUIState.FirstRun -> {
+                            findNavController().navigate(
+                                SplashFragmentDirections.actionSplashFragmentToOnBoardingFragment("Splash")
+                            )
+                        }
+                        SplashUIState.NotFirstRun -> {}
+                        SplashUIState.LoggedIn -> {
+                            view?.findNavController()?.navigate(R.id.action_splashFragment_to_baseActivity)
+                            activity?.finish()
+                        }
+                        SplashUIState.NotLoggedIn -> {
+                            findNavController().navigate(R.id.action_splash_destination_to_loginFragment)
+                        }
+                        else -> {}
                     }
                 }
             }
