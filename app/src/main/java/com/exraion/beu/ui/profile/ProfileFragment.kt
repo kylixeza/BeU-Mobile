05bdc3edeea19878085b9e10b851366300e59a03
ACER <loilyxtra@gmail.com>
2023-02-25 16:41:06 +0700

    designed profile fragment

--- app/src/main/java/com/exraion/beu/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/exraion/beu/ui/profile/ProfileFragment.kt
@@ -1,13 +1,7 @@
 package com.exraion.beu.ui.profile
 
-import androidx.lifecycle.ViewModelProvider
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
-import com.exraion.beu.R
 import com.exraion.beu.base.BaseFragment
 import com.exraion.beu.databinding.FragmentProfileBinding
 import com.exraion.beu.util.ScreenOrientation
