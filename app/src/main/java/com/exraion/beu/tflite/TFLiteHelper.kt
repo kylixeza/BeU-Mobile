dd1a8b344cae48c006218959b87cf9fa12c8c398
ACER <loilyxtra@gmail.com>
2023-02-28 14:55:15 +0700

    added camera feature

---
+++ app/src/main/java/com/exraion/beu/tflite/TFLiteHelper.kt
@@ -0,0 +1,129 @@
+package com.exraion.beu.tflite
+
+import android.app.Activity
+import android.content.Context
+import android.graphics.Bitmap
+import org.tensorflow.lite.DataType
+import org.tensorflow.lite.Interpreter
+import org.tensorflow.lite.support.common.FileUtil
+import org.tensorflow.lite.support.common.TensorOperator
+import org.tensorflow.lite.support.common.TensorProcessor
+import org.tensorflow.lite.support.common.ops.NormalizeOp
+import org.tensorflow.lite.support.image.ImageProcessor
+import org.tensorflow.lite.support.image.TensorImage
+import org.tensorflow.lite.support.image.ops.ResizeOp
+import org.tensorflow.lite.support.image.ops.ResizeWithCropOrPadOp
+import org.tensorflow.lite.support.label.TensorLabel
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
+import java.io.FileInputStream
+import java.nio.MappedByteBuffer
+import java.nio.channels.FileChannel
+import java.util.*
+
+
+class TFLiteHelper(
+    private val context: Context
+) {
+    
+    private var imageSizeX = 0
+    private var imageSizeY = 0
+    
+    private var labels: List<String>? = null
+    private var tflite: Interpreter? = null
+    
+    private var inputImageBuffer: TensorImage? = null
+    private var outputProbabilityBuffer: TensorBuffer? = null
+    private var probabilityProcessor: TensorProcessor? = null
+    
+    companion object {
+        private const val IMAGE_MEAN = 0.0f
+        private const val IMAGE_STD = 1.0f
+    
+        private const val PROBABILITY_MEAN = 0.0f
+        private const val PROBABILITY_STD = 255.0f
+    
+        private const val MODEL_PATH = "beu_v5.tflite"
+        private const val LABELS_PATH = "beu_labels.txt"
+    }
+    
+    init {
+        try {
+            val opt = Interpreter.Options()
+            tflite = loadModelFile(context as Activity)?.let { Interpreter(it, opt) }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+    
+    fun classifyImage(bitmap: Bitmap?) {
+        val imageTensorIndex = 0
+        val imageShape = tflite!!.getInputTensor(imageTensorIndex).shape() // {1, height, width, 3}
+        imageSizeY = imageShape[1]
+        imageSizeX = imageShape[2]
+        val imageDataType: DataType = tflite!!.getInputTensor(imageTensorIndex).dataType()
+        val probabilityTensorIndex = 0
+        val probabilityShape =
+            tflite!!.getOutputTensor(probabilityTensorIndex).shape() // {1, NUM_CLASSES}
+        val probabilityDataType: DataType =
+            tflite!!.getOutputTensor(probabilityTensorIndex).dataType()
+        inputImageBuffer = TensorImage(imageDataType)
+        outputProbabilityBuffer =
+            TensorBuffer.createFixedSize(probabilityShape, probabilityDataType)
+        probabilityProcessor = TensorProcessor.Builder().add(getPostProcessNormalizeOp()).build()
+        inputImageBuffer = loadImage(bitmap!!)
+        tflite!!.run(inputImageBuffer!!.buffer, outputProbabilityBuffer!!.buffer.rewind())
+    }
+    
+    private fun loadModelFile(activity: Activity): MappedByteBuffer? {
+        val fileDescriptor = activity.assets.openFd(MODEL_PATH)
+        val inputStream = FileInputStream(fileDescriptor.fileDescriptor)
+        val fileChannel: FileChannel = inputStream.channel
+        val startOffset = fileDescriptor.startOffset
+        val declaredLength = fileDescriptor.declaredLength
+        return fileChannel.map(FileChannel.MapMode.READ_ONLY, startOffset, declaredLength)
+    }
+    
+    private fun loadImage(bitmap: Bitmap): TensorImage? {
+        inputImageBuffer!!.load(bitmap)
+        val cropSize = bitmap.width.coerceAtMost(bitmap.height)
+        val imageProcessor: ImageProcessor = ImageProcessor.Builder()
+            .add(ResizeWithCropOrPadOp(cropSize, cropSize))
+            .add(ResizeOp(imageSizeX, imageSizeY, ResizeOp.ResizeMethod.BILINEAR))
+            .add(getPreprocessNormalizeOp())
+            .build()
+        return imageProcessor.process(inputImageBuffer)
+    }
+    
+    private fun getPreprocessNormalizeOp(): TensorOperator {
+        return NormalizeOp(IMAGE_MEAN, IMAGE_STD)
+    }
+    
+    private fun getPostProcessNormalizeOp(): TensorOperator {
+        return NormalizeOp(PROBABILITY_MEAN, PROBABILITY_STD)
+    }
+    
+    fun showResult(): String? {
+        labels = try {
+            FileUtil.loadLabels(context, LABELS_PATH)
+        } catch (e: java.lang.Exception) {
+            e.printStackTrace()
+            return null
+        }
+        val labeledProbability =
+            labels?.let {
+                TensorLabel(it, probabilityProcessor!!.process(outputProbabilityBuffer))
+                    .mapWithFloatValue
+            }
+        val maxValueInMap: Float = Collections.max(labeledProbability!!.values)
+        var result: String? = null
+        labeledProbability.forEach { (key, value) ->
+            if (value == maxValueInMap) {
+                result = key
+            }
+        }
+        
+        return result
+    }
+    
+    
+}
--- app/src/main/java/com/exraion/beu/ui/recognition/ImageRecognitionFragment.kt
+++ app/src/main/java/com/exraion/beu/ui/recognition/ImageRecognitionFragment.kt
@@ -1,14 +1,45 @@
 package com.exraion.beu.ui.recognition
 
+import android.Manifest
+import android.R.attr.bitmap
+import android.app.Activity.RESULT_OK
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
+import android.provider.MediaStore
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.camera.core.*
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.core.content.ContextCompat
 import androidx.navigation.fragment.findNavController
-import com.exraion.beu.R
 import com.exraion.beu.base.BaseFragment
 import com.exraion.beu.databinding.FragmentImageRecognitionBinding
+import com.exraion.beu.tflite.TFLiteHelper
 import com.exraion.beu.util.ScreenOrientation
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import java.io.IOException
+import java.util.concurrent.Executors
+
 
 class ImageRecognitionFragment : BaseFragment<FragmentImageRecognitionBinding>() {
+    private val cameraExecutor = Executors.newSingleThreadExecutor()
+    private var imagePreview: Preview? = null
+    private var imageCapture: ImageCapture? = null
+    private var cameraControl: CameraControl? = null
+    private var cameraInfo: CameraInfo? = null
+    
+    private lateinit var tfLiteHelper: TFLiteHelper
+    
+    companion object {
+        private const val PHOTO_EXTENSION = ".jpg"
+        
+        private const val REQUEST_CODE_PERMISSIONS = 10
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+        
+        private const val REQUEST_CODE = 1
+    }
+    
     override fun inflateViewBinding(container: ViewGroup?): FragmentImageRecognitionBinding {
         return FragmentImageRecognitionBinding.inflate(layoutInflater, container, false)
     }
@@ -16,12 +47,142 @@ class ImageRecognitionFragment : BaseFragment<FragmentImageRecognitionBinding>()
     override fun determineScreenOrientation(): ScreenOrientation = ScreenOrientation.PORTRAIT
     
     override fun FragmentImageRecognitionBinding.binder() {
+        tfLiteHelper = TFLiteHelper(requireContext())
+        if (allPermissionsGranted()) {
+            startCamera()
+        } else {
+            requestPermissions(
+                REQUIRED_PERMISSIONS,
+                REQUEST_CODE_PERMISSIONS
+            )
+        }
+        
+        ivCameraCapture.setOnClickListener {
+            takePhoto()
+        }
+        
+        ivPickGallery.setOnClickListener {
+            takeFromGallery()
+        }
+    }
     
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
+        val cameraSelector = CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK).build()
+        cameraProviderFuture.addListener({
+            imagePreview = Preview.Builder().apply {
+                setTargetAspectRatio(AspectRatio.RATIO_16_9)
+                setTargetRotation(binding!!.previewView.display.rotation)
+            }.build()
+            
+            imageCapture = ImageCapture.Builder().apply {
+                setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY)
+                setFlashMode(ImageCapture.FLASH_MODE_AUTO)
+            }.build()
+            
+            val cameraProvider = cameraProviderFuture.get()
+            val camera = cameraProvider.bindToLifecycle(
+                this,
+                cameraSelector,
+                imagePreview,
+                // imageAnalysis,
+                imageCapture,
+            )
+            binding!!.previewView.implementationMode = PreviewView.ImplementationMode.COMPATIBLE
+            imagePreview?.setSurfaceProvider(binding!!.previewView.surfaceProvider)
+            cameraControl = camera.cameraControl
+            cameraInfo = camera.cameraInfo
+        }, ContextCompat.getMainExecutor(requireContext()))
+    }
+    
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(requireContext(), it) == PackageManager.PERMISSION_GRANTED
+    }
+    
+    private fun takePhoto() {
+        imageCapture!!.takePicture(cameraExecutor, object : ImageCapture.OnImageCapturedCallback() {
+            override fun onCaptureSuccess(image: ImageProxy) {
+                val buffer = image.planes[0].buffer
+                val bytes = ByteArray(buffer.capacity())
+                buffer.get(bytes)
+                val bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
+                tfLiteHelper.classifyImage(bitmap)
+                activity?.runOnUiThread {
+                    val label = tfLiteHelper.showResult()
+                    Toast.makeText(requireContext(), label, Toast.LENGTH_SHORT).show()
+                }
+                image.close()
+            }
+        
+            override fun onError(exception: ImageCaptureException) {
+                Toast.makeText(requireContext(), "Error capturing photo: ${exception.message}", Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+    
+    private fun takeFromGallery() {
+        val selectType = "image/*"
+        val selectPicture = "Select Picture"
+    
+        val intent = Intent()
+        intent.type = selectType
+        intent.action = Intent.ACTION_GET_CONTENT
+        startActivityForResult(Intent.createChooser(intent, selectPicture), REQUEST_CODE)
     }
     
     override fun onBackPressedBehaviour() {
         findNavController().navigate(
             ImageRecognitionFragmentDirections.actionNavigationImageRecognitionToNavigationHome()
         )
+    }
+    
+    @Deprecated("Deprecated in Java")
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                startCamera()
+            } else {
+                Toast.makeText(
+                    requireContext(),
+                    "Permissions not granted by the user.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+            return
+        }
+    }
+    
+    @Deprecated("Deprecated in Java", ReplaceWith(
+        "super.onActivityResult(requestCode, resultCode, data)",
+        "com.exraion.beu.base.BaseFragment"
+    )
+    )
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode === REQUEST_CODE && resultCode === RESULT_OK && data != null) {
+            val imageUri = data.data
+            try {
+                val bitmap = MediaStore.Images.Media.getBitmap(activity?.contentResolver, imageUri)
+                tfLiteHelper.classifyImage(bitmap)
+                activity?.runOnUiThread {
+                    val label = tfLiteHelper.showResult()
+                    Toast.makeText(requireContext(), label, Toast.LENGTH_SHORT).show()
+                }
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
+        
+    }
+    
+    
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
     }
 }
