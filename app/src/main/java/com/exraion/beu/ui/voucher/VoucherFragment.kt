6fcb9a430d97f74b06a888ff284fba096e3bf727
ACER <loilyxtra@gmail.com>
2023-04-25 23:42:11 +0700

    added voucher shop flows

--- app/src/main/java/com/exraion/beu/ui/voucher/VoucherFragment.kt
+++ app/src/main/java/com/exraion/beu/ui/voucher/VoucherFragment.kt
@@ -1,12 +1,26 @@
 package com.exraion.beu.ui.voucher
 
 import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.exraion.beu.adapter.voucher.VoucherAdapter
 import com.exraion.beu.base.BaseFragment
 import com.exraion.beu.databinding.FragmentVoucherBinding
 import com.exraion.beu.util.ScreenOrientation
+import com.exraion.beu.util.UIState
+import io.github.tonnyl.light.Light
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+import org.koin.android.ext.android.inject
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class VoucherFragment : BaseFragment<FragmentVoucherBinding>() {
+
+    private val viewModel by viewModel<VoucherViewModel>()
+    private val productAdapter by inject<VoucherAdapter>()
+    private val shippingAdapter by inject<VoucherAdapter>()
+
     override fun inflateViewBinding(container: ViewGroup?): FragmentVoucherBinding {
         return FragmentVoucherBinding.inflate(layoutInflater, container, false)
     }
@@ -16,7 +30,46 @@ class VoucherFragment : BaseFragment<FragmentVoucherBinding>() {
     }
     
     override fun FragmentVoucherBinding.binder() {
-    
+
+        rvShipping.apply {
+            adapter = shippingAdapter
+            layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+        }
+
+        rvProduct.apply {
+            adapter = productAdapter
+            layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+        }
+
+        lifecycleScope.launch {
+            viewModel.uiState.collect {
+                when(it) {
+                    UIState.IDLE -> doNothing()
+                    UIState.LOADING -> { }
+                    UIState.SUCCESS -> { }
+                    UIState.ERROR -> Light.error(requireView(), viewModel.message, Light.LENGTH_SHORT).show()
+                    UIState.EMPTY -> { }
+                }
+            }
+        }
+
+        lifecycleScope.launch {
+            viewModel.shippingVoucher.collectLatest {
+                shippingAdapter.submitList(it)
+            }
+        }
+
+        lifecycleScope.launch {
+            viewModel.productVoucher.collectLatest {
+                productAdapter.submitList(it)
+            }
+        }
+
+        appBarVoucher.ivMyVoucher.setOnClickListener {
+            findNavController().navigate(
+                VoucherFragmentDirections.actionNavigationVoucherToNavigationMyVoucher()
+            )
+        }
     }
     
     override fun onBackPressedBehaviour() {
@@ -24,5 +77,4 @@ class VoucherFragment : BaseFragment<FragmentVoucherBinding>() {
             VoucherFragmentDirections.actionNavigationVoucherToNavigationHome()
         )
     }
-    
 }
--- app/src/main/java/com/exraion/beu/ui/voucher/VoucherViewModel.kt
+++ app/src/main/java/com/exraion/beu/ui/voucher/VoucherViewModel.kt
@@ -1,7 +1,56 @@
 package com.exraion.beu.ui.voucher
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.exraion.beu.data.repository.voucher.VoucherRepository
+import com.exraion.beu.data.util.Resource
+import com.exraion.beu.model.VoucherList
+import com.exraion.beu.util.UIState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 
-class VoucherViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class VoucherViewModel(
+    private val repository: VoucherRepository
+) : ViewModel() {
+
+    private val _isExpanded = MutableStateFlow(false)
+    val isExpanded = _isExpanded.asStateFlow()
+
+    private val _uiState = MutableStateFlow(UIState.IDLE)
+    val uiState = _uiState.asStateFlow()
+
+    private val _shippingVoucher = MutableStateFlow(listOf<VoucherList>())
+    val shippingVoucher = _shippingVoucher.asStateFlow()
+
+    private val _productVoucher = MutableStateFlow(listOf<VoucherList>())
+    val productVoucher = _productVoucher.asStateFlow()
+
+    var message: String = ""
+
+    init {
+        _uiState.value = UIState.LOADING
+        viewModelScope.launch {
+            repository.fetchAvailableVouchers().collect {
+                when(it) {
+                    is Resource.Success -> {
+                        _uiState.value = UIState.SUCCESS
+                        _shippingVoucher.value = it.data?.shipping ?: emptyList()
+                        _productVoucher.value = it.data?.product ?: emptyList()
+                    }
+                    is Resource.Empty -> _uiState.value = UIState.EMPTY
+                    is Resource.Error -> {
+                        _uiState.value = UIState.ERROR
+                        message = it.message.orEmpty()
+                    }
+                    else -> {}
+                }
+            }
+        }
+    }
+
+    fun showMoreItems() {
+        _isExpanded.value = !_isExpanded.value
+    }
+
 }
