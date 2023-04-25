754067335e3bfa89753bea3d0a37fd88eead93c5
ACER <loilyxtra@gmail.com>
2023-04-25 12:26:07 +0700

    fixed conflict strategy

--- app/src/main/java/com/exraion/beu/data/source/local/database/room/BeUDao.kt
+++ app/src/main/java/com/exraion/beu/data/source/local/database/room/BeUDao.kt
@@ -2,6 +2,7 @@ package com.exraion.beu.data.source.local.database.room
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.exraion.beu.data.source.local.database.entity.UserEntity
 import kotlinx.coroutines.flow.Flow
@@ -9,7 +10,7 @@ import kotlinx.coroutines.flow.Flow
 @Dao
 interface BeUDao {
     
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertUser(user: UserEntity)
     
     @Query("SELECT * FROM user_table WHERE token = :token")
