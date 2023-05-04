f5394d22b2dc01e4f5ff45f391d544225974a7d4
ACER <loilyxtra@gmail.com>
2023-05-04 23:40:53 +0700

    added some new extensions

--- app/src/main/java/com/exraion/beu/util/Ext.kt
+++ app/src/main/java/com/exraion/beu/util/Ext.kt
@@ -2,12 +2,14 @@ package com.exraion.beu.util
 
 fun doNothing() = Unit
 
-infix fun <T> T?.isNotNullDo(doSomething: (T) -> Unit): T? {
+infix fun <T> T.isEqualTo(other: T): Boolean = this == other
+
+infix fun <T> T?.isNotNullThen(doSomething: (T) -> Unit): T? {
     if (this != null) doSomething(this)
     return this
 }
 
-infix fun <T> T?.isNullDo(doSomething: () -> Unit): T? {
+infix fun <T> T?.isNullThen(doSomething: () -> Unit): T? {
     if (this == null) doSomething()
     return this
 }
@@ -18,12 +20,20 @@ infix fun<T> Boolean.then(block: () -> T): ThenOtherwise<T> {
     return ThenOtherwise(this, call)
 }
 
-infix fun<T: Any> ThenOtherwise<T>.otherwise(block: () -> T): T =
+infix fun<T: Any> ThenOtherwise<T?>.otherwise(block: () -> T?): T? =
     if (!currentBooleanState) block()
     else data!!
 
+infix fun<T: Any> ThenOtherwise<T>.otherwise(block: () -> T): T =
+    if (!currentBooleanState) block()
+    else data!!
 
 data class ThenOtherwise<T>(
     val currentBooleanState: Boolean,
     val data: T?
 )
+
+infix fun Number.isGreaterThan(other: Number): Boolean = this.toDouble() > other.toDouble()
+infix fun Number.isLessThan(other: Number): Boolean = this.toDouble() < other.toDouble()
+infix fun Number.isGreaterThanOrEqual(other: Number): Boolean = this.toDouble() >= other.toDouble()
+infix fun Number.isLessThanOrEqual(other: Number): Boolean = this.toDouble() <= other.toDouble()
