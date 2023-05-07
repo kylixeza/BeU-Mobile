7470d874c7163735eaf2679cb105cd10787f7a95
ACER <loilyxtra@gmail.com>
2023-05-07 22:23:46 +0700

    added state when collecting data

--- app/src/main/java/com/exraion/beu/ui/leaderboard/LeaderboardFragment.kt
+++ app/src/main/java/com/exraion/beu/ui/leaderboard/LeaderboardFragment.kt
@@ -11,8 +11,13 @@ import com.exraion.beu.common.initLinearVertical
 import com.exraion.beu.databinding.DialogRankBinding
 import com.exraion.beu.databinding.FragmentLeaderboardBinding
 import com.exraion.beu.util.ScreenOrientation
-import com.exraion.beu.util.UIState
+import com.exraion.beu.util.hideWhen
+import com.exraion.beu.util.isErrorDo
+import com.exraion.beu.util.isLoading
+import com.exraion.beu.util.isSuccess
+import com.exraion.beu.util.showWhen
 import io.github.tonnyl.light.Light
+import kotlinx.coroutines.launch
 import org.koin.android.ext.android.inject
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -33,27 +38,24 @@ class LeaderboardFragment : BaseFragment<FragmentLeaderboardBinding>() {
     
     override fun FragmentLeaderboardBinding.binder() {
         dialogRankBinding = DialogRankBinding.inflate(layoutInflater)
-        rvleaderboard.initLinearVertical(requireContext(), adapter)
+        rvLeaderboard.initLinearVertical(requireContext(), adapter)
         
-        lifecycleScope.launchWhenStarted {
+        lifecycleScope.launch {
             viewModel.uiState.collect {
-                when(it) {
-                    UIState.IDLE -> doNothing()
-                    UIState.LOADING -> this@binder.appBarLeaderboard.ivInfo.hide()
-                    UIState.SUCCESS -> this@binder.appBarLeaderboard.ivInfo.show()
-                    UIState.ERROR -> Light.error(binding!!.root, viewModel.message, Light.LENGTH_SHORT).show()
-                    UIState.EMPTY -> doNothing()
-                }
+                appBarLeaderboard.ivInfo showWhen it.isSuccess() hideWhen it.isLoading()
+                pbLeaderboard showWhen it.isLoading() hideWhen it.isSuccess()
+                rvLeaderboard showWhen it.isSuccess() hideWhen it.isLoading()
+                it.isErrorDo { Light.error(root, viewModel.message, Light.LENGTH_SHORT).show() }
             }
         }
         
-        lifecycleScope.launchWhenStarted {
+        lifecycleScope.launch {
             viewModel.leaderboard.collect {
                 adapter.submitList(it)
             }
         }
         
-        lifecycleScope.launchWhenStarted {
+        lifecycleScope.launch {
             viewModel.myRank.collect {
                 dialog = requireContext().buildLeaderboardDialog(dialogRankBinding, it)
             }
