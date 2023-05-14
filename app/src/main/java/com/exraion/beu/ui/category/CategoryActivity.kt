915321184a35d1a3cc707f4d848e2afb631ba935
ACER <loilyxtra@gmail.com>
2023-05-14 15:35:27 +0700

    added category logic

--- app/src/main/java/com/exraion/beu/ui/category/CategoryActivity.kt
+++ app/src/main/java/com/exraion/beu/ui/category/CategoryActivity.kt
@@ -1,10 +1,30 @@
 package com.exraion.beu.ui.category
 
+import androidx.lifecycle.lifecycleScope
+import com.exraion.beu.adapter.menu_list_vertical.MenuListVerticalAdapter
 import com.exraion.beu.base.BaseActivity
+import com.exraion.beu.common.initLinearVertical
 import com.exraion.beu.databinding.ActivityCategoryBinding
 import com.exraion.beu.util.ScreenOrientation
+import com.exraion.beu.util.hideWhen
+import com.exraion.beu.util.isErrorDo
+import com.exraion.beu.util.isLoading
+import com.exraion.beu.util.isSuccess
+import com.exraion.beu.util.showWhen
+import io.github.tonnyl.light.Light
+import kotlinx.coroutines.launch
+import org.koin.android.ext.android.inject
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class CategoryActivity : BaseActivity<ActivityCategoryBinding>() {
+
+    private val viewModel by viewModel<CategoryViewModel>()
+    private val adapter by inject<MenuListVerticalAdapter>()
+
+    companion object {
+        const val CATEGORY = "category"
+    }
+
     override fun inflateViewBinding(): ActivityCategoryBinding {
         return ActivityCategoryBinding.inflate(layoutInflater)
     }
@@ -14,7 +34,30 @@ class CategoryActivity : BaseActivity<ActivityCategoryBinding>() {
     }
     
     override fun ActivityCategoryBinding.binder() {
-    
+        val category = intent.getStringExtra(CATEGORY).orEmpty()
+
+        appBarCategory.apply {
+            tvTitle.text = category
+            ivArrowBack.setOnClickListener { finish() }
+            ivFavorite.hide()
+        }
+
+        rvCategory.initLinearVertical(this@CategoryActivity, adapter)
+
+        viewModel.fetchCategorizedMenus(category)
+
+        lifecycleScope.launch {
+            viewModel.uiState.collect {
+                pbCategory showWhen it.isLoading() hideWhen it.isSuccess()
+                rvCategory showWhen it.isSuccess() hideWhen it.isLoading()
+                it.isErrorDo { Light.error(root, viewModel.message, Light.LENGTH_SHORT).show() }
+            }
+        }
+
+        lifecycleScope.launch {
+            viewModel.menus.collect {
+                adapter.submitList(it)
+            }
+        }
     }
-    
 }
--- app/src/main/java/com/exraion/beu/ui/category/CategoryViewModel.kt
+++ app/src/main/java/com/exraion/beu/ui/category/CategoryViewModel.kt
@@ -1,7 +1,45 @@
 package com.exraion.beu.ui.category
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.exraion.beu.data.repository.menu.MenuRepository
+import com.exraion.beu.data.util.Resource
+import com.exraion.beu.model.MenuList
+import com.exraion.beu.util.UIState
+import com.exraion.beu.util.doNothing
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 
-class CategoryViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class CategoryViewModel(
+    private val repository: MenuRepository
+) : ViewModel() {
+
+    private val _uiState = MutableStateFlow(UIState.IDLE)
+    val uiState = _uiState.asStateFlow()
+
+    private val _menus = MutableStateFlow(listOf<MenuList>())
+    val menus = _menus.asStateFlow()
+
+    var message: String = ""
+
+    fun fetchCategorizedMenus(category: String) {
+        viewModelScope.launch {
+            _uiState.value = UIState.LOADING
+            repository.fetchCategorizedMenus(category).collect {
+                when(it) {
+                    is Resource.Success -> {
+                        _menus.value = it.data ?: listOf()
+                        _uiState.value = UIState.SUCCESS
+                    }
+                    is Resource.Error -> {
+                        message = it.message ?: ""
+                        _uiState.value = UIState.ERROR
+                    }
+                    else -> doNothing()
+                }
+            }
+        }
+    }
+
 }
