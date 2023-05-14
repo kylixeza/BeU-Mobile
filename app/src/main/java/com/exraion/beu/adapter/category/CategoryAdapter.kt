eae881daa3c579f3c9f9be94050f6b30fbe6940e
ACER <loilyxtra@gmail.com>
2023-05-14 15:39:36 +0700

    intended to category activity

--- app/src/main/java/com/exraion/beu/adapter/category/CategoryAdapter.kt
+++ app/src/main/java/com/exraion/beu/adapter/category/CategoryAdapter.kt
@@ -1,10 +1,12 @@
 package com.exraion.beu.adapter.category
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import com.exraion.beu.base.BaseRecyclerViewAdapter
 import com.exraion.beu.databinding.ItemListCategoryBinding
+import com.exraion.beu.ui.category.CategoryActivity
 
 class CategoryAdapter : BaseRecyclerViewAdapter<ItemListCategoryBinding, String>() {
     override fun inflateViewBinding(parent: ViewGroup): ItemListCategoryBinding {
@@ -17,6 +19,11 @@ class CategoryAdapter : BaseRecyclerViewAdapter<ItemListCategoryBinding, String>
     override val binder: (String, ItemListCategoryBinding) -> Unit
         get() = { data, view ->
             view.tvCategory.text = data
+            itemView.setOnClickListener {
+                val intent = Intent(itemView.context, CategoryActivity::class.java)
+                intent.putExtra(CategoryActivity.CATEGORY, data)
+                itemView.context.startActivity(intent)
+            }
         }
     
 }
